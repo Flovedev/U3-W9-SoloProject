650ddbb67b15e86651e02e13b9ab50c52e72b315
Flovedev <flove.dev@gmail.com>
2023-02-03 16:02:41 +0000

    updates

--- src/components/JumbotronComponent.jsx
+++ src/components/JumbotronComponent.jsx
@@ -15,8 +15,8 @@ const JumbotronComponent = () => {
             <Button variant="light" className="px-5 py-3">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="50"
-                height="50"
+                width="40"
+                height="40"
                 fill="currentColor"
                 class="bi bi-play-fill mr-2"
                 viewBox="0 0 16 16"
@@ -26,6 +26,17 @@ const JumbotronComponent = () => {
               Play
             </Button>{" "}
             <Button variant="secondary" className="px-5 py-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="40"
+                height="40"
+                fill="currentColor"
+                class="bi bi-info-circle mr-2"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+              </svg>
               More info
             </Button>{" "}
           </div>
