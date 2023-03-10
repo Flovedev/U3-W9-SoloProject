65f2fd77891521d7119316e12fcb5d6cfbeca059
Flovedev <flove.dev@gmail.com>
2023-03-10 14:19:22 +0000

    update fetch

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -14,32 +14,20 @@ class MainComponent extends Component {
     isError: false,
   };
 
-  fetchMovies = async (endpoint) => {
+  fetchAPI = async () => {
     try {
-      let res = await fetch(url + endpoint);
-
+      const res = await fetch(process.env.REACT_APP_BE_URL + "/medias");
       if (res.ok) {
-        let data = await res.json();
-        // console.log(data.Search);
-        endpoint === "harry+potter" &&
-          this.setState({
-            harry: data.Search,
-            isLoading: false,
-          });
-        endpoint === "star+wars" &&
-          this.setState({
-            star: data.Search,
-            isLoading: false,
-          });
-        endpoint === "lord+of+the+rings" &&
-          this.setState({
-            lord: data.Search,
-            isLoading: false,
-          });
-      } else {
+        const data = await res.json();
+        console.log(data);
+        let harryResult = data.filter((e) => e.Title.includes("Harry"));
+        let starResult = data.filter((e) => e.Title.includes("Star"));
+        let lordResult = data.filter((e) => e.Title.includes("Lord"));
         this.setState({
+          harry: harryResult,
+          star: starResult,
+          lord: lordResult,
           isLoading: false,
-          isError: true,
         });
       }
     } catch (error) {
@@ -47,10 +35,44 @@ class MainComponent extends Component {
     }
   };
 
+  // fetchMovies = async (endpoint) => {
+  //   try {
+  //     let res = await fetch(url + endpoint);
+
+  //     if (res.ok) {
+  //       let data = await res.json();
+  //       // console.log(data.Search);
+  //       endpoint === "harry+potter" &&
+  //         this.setState({
+  //           harry: data.Search,
+  //           isLoading: false,
+  //         });
+  //       endpoint === "star+wars" &&
+  //         this.setState({
+  //           star: data.Search,
+  //           isLoading: false,
+  //         });
+  //       endpoint === "lord+of+the+rings" &&
+  //         this.setState({
+  //           lord: data.Search,
+  //           isLoading: false,
+  //         });
+  //     } else {
+  //       this.setState({
+  //         isLoading: false,
+  //         isError: true,
+  //       });
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
   componentDidMount() {
-    this.fetchMovies("harry+potter");
-    this.fetchMovies("star+wars");
-    this.fetchMovies("lord+of+the+rings");
+    // this.fetchMovies("harry+potter");
+    // this.fetchMovies("star+wars");
+    // this.fetchMovies("lord+of+the+rings");
+    this.fetchAPI();
   }
   render() {
     return (
